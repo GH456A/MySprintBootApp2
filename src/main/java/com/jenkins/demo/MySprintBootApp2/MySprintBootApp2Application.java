5193541b9d2f4c6c6543a3da28e3739c7719a818
Ming <mctwok@gmail.com>
2024-07-03 16:10:30 -0700

    added 2 new test cases

--- src/main/java/com/jenkins/demo/MySprintBootApp2/MySprintBootApp2Application.java
+++ src/main/java/com/jenkins/demo/MySprintBootApp2/MySprintBootApp2Application.java
@@ -8,6 +8,10 @@ public class MySprintBootApp2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySprintBootApp2Application.class, args);
+	
+//		MyAppController app = new MyAppController();
+//		System.out.println(app.getLatestNewsHeadLines());
+	
 	}
 
 }
--- src/test/java/com/jenkins/demo/MySprintBootApp2/MySprintBootApp2ApplicationTests.java
+++ src/test/java/com/jenkins/demo/MySprintBootApp2/MySprintBootApp2ApplicationTests.java
@@ -1,9 +1,11 @@
 package com.jenkins.demo.MySprintBootApp2;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.jenkins.demo.MySprintBootApp2.MyAppController;
@@ -22,6 +24,11 @@ class MySprintBootApp2ApplicationTests {
 	@Test
 	void newsLoads() {
 		assertNotNull(myAppController.getLatestNewsHeadLines());
+	}
+	
+	@Test
+	void testgetLatestNewsHeadLines() {
+		assertEquals("Six Flags, Cedar Fair merge in huge theme park deal",myAppController.getLatestNewsHeadLines());
 	}
 	
 }
